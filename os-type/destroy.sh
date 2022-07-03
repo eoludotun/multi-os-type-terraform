16bb7fdb29309a2ab16d34ac617e4cee1a933f49
eoludotun <ebenezer.software.tester@gmail.com>
2022-07-03 01:06:34 +0000

    not a key

--- os-type/destroy.sh
+++ os-type/destroy.sh
@@ -1,3 +1,4 @@
+terraform init
 terraform destroy -auto-approve
 rm -rf .terraform terraform.tfstate terraform.tfstate.backup
 
--- os-type/execute.sh
+++ os-type/execute.sh
@@ -1,5 +1,6 @@
+
 terraform init
 terraform plan
-terraform apply -auto-approve
-terraform output -raw admin_ssh_key_public
-terraform output -raw admin_ssh_key_private
+terraform apply | -auto-approve
+#terraform output -raw admin_ssh_key_public
+#terraform output -raw admin_ssh_key_private
